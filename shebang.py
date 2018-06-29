5affeb674ff2149f2fa4dd91e889c16fbac1bde8
Vitor Liberato <vitorcolombo@gmail.com>
2018-06-28 21:48:40 -0300

    Aula 4 - 28/06/2018

--- shebang.py
+++ shebang.py
@@ -14,4 +14,26 @@ if conteudo[0] != '\#\!/usr/bin/python3\n':
     with open(nome,'w') as arquivo:
         for linha in conteudo:
             arquivo.write(linha)
+
+
+
+
+# http://dontpad.com/520/noturno
+#!/usr/bin/python3
+from subprocess import subprocess
+
+name = input("digite o nome do arquivo")
+try:
+    with open(name, 'r') as arquivo:
+        conteudo = arquivo.readlines()
+        conteudo.insert(0, '#!/usr/bin/python3\n')
+    with open(name, 'w+') as arquivo:
+        for x in conteudo:
+            arquivo.write(x)
+
+except FileNotFoundError:
+    with open(name, 'a') as arquivo:
+        arquivo.write('#!/usr/bin/python3\n')
+
+run(['sudo', 'chmod', '+x', name])
             
